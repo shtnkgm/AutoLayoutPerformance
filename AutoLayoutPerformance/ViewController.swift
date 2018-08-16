c27b326096a03b669b47c66ca549bd8efeaa626c
shtnkgm <nakagami.shota@gmail.com>
2018-08-16 23:05:57 +0900

    ViewControllerをコードベースで表示

--- AutoLayoutPerformance/ViewController.swift
+++ AutoLayoutPerformance/ViewController.swift
@@ -9,12 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = .blue
     }
-
-
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
